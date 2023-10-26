053b6e5157e0572f0018607954e7e20b5070d3ad
Anita Kuresi <aniber2.ak@gmail.com>
2023-10-26 17:27:01 +0300

    therd commit

--- src/Rock-Scissor-Paper-main/script.js
+++ src/Rock-Scissor-Paper-main/script.js
@@ -7,6 +7,8 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+const jsConfetti = new JSConfetti();
+const canvas = document.querySelector(".confetti");
 
 
 function getComputerChoice(){
@@ -29,6 +31,8 @@ function win(userChoice, computerChoice){
     result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + ". You won!";
     userChoice_div.classList.add("green-glow");
     setTimeout(function(){userChoice_div.classList.remove("green-glow")},500);
+    jsConfetti.addConfetti()
+
 }
 
 function lose(userChoice, computerChoice){
